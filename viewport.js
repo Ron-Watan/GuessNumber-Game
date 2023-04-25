077b96db392c473a9118811ae112e8d400c58dbd
Ron-Watan <ronwatan@outlook.com>
2023-04-24 23:42:07 -0700

    Delete dev.files

--- viewport.js
+++
@@ -1,8 +0,0 @@
-const currentViewPort = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-
-window.addEventListener('resize', viewPortSize);
-
-function viewPortSize() {
-  location.reload();
-}
-document.getElementById('current--vh').innerHTML = currentViewPort;
